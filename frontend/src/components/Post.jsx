5a25a48c00c894964b2c7f7e41dd17aa0c55f94e
rasikarakhewar3010 <rasikarakhewar3010@gmail.com>
2025-01-10 22:11:31 +0530

    like post function added

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -15,21 +15,11 @@ const Post = ({ post }) => {
     const [text, setText] = useState("");
     const [open, setopen] = useState(false);
     const { user } = useSelector(store => store.auth);
-    const {posts} = useSelector(store=>store.post);
+    const { posts } = useSelector(store => store.post);
+    const [liked, setLiked] = useState(post.likes.includes(user?._id) || false);
+    const [postLike, setPostLike] = useState(post.likes.length);
     const dispatch = useDispatch();
 
-    const deletePosthandler = async ()=>{
-        try {
-            const res = await axios.delete(`http://localhost:8000/api/v1/post/delete/${post?._id}`, {withCredentials:true});
-            if(res.data.success){
-                const updatedPostData = posts.filter((postItem)=> postItem?._id!= post?._id);
-                dispatch(setPosts(updatedPostData))
-                toast.success(res.data.message)
-            }
-        } catch (error) {
-            toast.error(error.response.data.message)
-        }
-    }
     const changeEventHandler = (e) => {
         const inputText = e.target.value;
         if (inputText.trim()) {
@@ -38,6 +28,48 @@ const Post = ({ post }) => {
             setText("");
         }
     }
+
+
+    const deletePosthandler = async () => {
+        try {
+            const res = await axios.delete(`http://localhost:8000/api/v1/post/delete/${post?._id}`, { withCredentials: true });
+            if (res.data.success) {
+                const updatedPostData = posts.filter((postItem) => postItem?._id != post?._id);
+                dispatch(setPosts(updatedPostData))
+                toast.success(res.data.message)
+            }
+        } catch (error) {
+            toast.error(error.response.data.message)
+        }
+    }
+
+
+    const likeOrDislikeHandler = async () => {
+        try {
+            const action = liked ? 'dislike' : 'like';
+            const res = await axios.get(`http://localhost:8000/api/v1/post/${post._id}/${action}`, { withCredentials: true });
+            console.log(res.data);
+            if (res.data.success) {
+                const updatedLikes = liked ? postLike - 1 : postLike + 1;
+                setPostLike(updatedLikes);
+                setLiked(!liked);
+
+                // update post
+                const updatedPostData = posts.map(p =>
+                    p._id === post._id ? {
+                        ...p,
+                        likes: liked ? p.likes.filter(id => id !== user._id) : [...p.likes, user._id]
+                    } : p
+                );
+                dispatch(setPosts(updatedPostData));
+                toast.success(res.data.message);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
     return (
         <div className="my-8 w-full max-w-sm mx-auto">
             <div className="flex items-center justify-between">
@@ -69,13 +101,15 @@ const Post = ({ post }) => {
 
             <div className="flex items-center justify-between my-2">
                 <div className="flex items-center gap-3">
-                    <FaRegHeart size={'22px'} className="cursor-pointer hover:text-gray-600" />
+                    {
+                        liked ? <FaHeart onClick={likeOrDislikeHandler} size={'24'} className='cursor-pointer text-red-600' /> : <FaRegHeart onClick={likeOrDislikeHandler} size={'22px'} className='cursor-pointer hover:text-gray-600' />
+                    }
                     <MessageCircle onClick={() => setopen(true)} className="cursor-pointer hover:text-gray-600" />
                     <Send className="cursor-pointer hover:text-gray-600" />
                 </div>
                 <Bookmark className="cursor-pointer hover:text-gray-600" />
             </div>
-            <span className="font-medium block mb-2">{post.likes.length} likes</span>
+            <span className="font-medium block mb-2">{postLike} likes</span>
             <p>
                 <span className="font-bold mr-2">{post.author?.username}</span>
                 {post.caption}
